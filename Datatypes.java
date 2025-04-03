a20ef00323c2adb91248cb2de1c911ea9df0ac9b
HarishRomario <155374957+HarishRomario@users.noreply.github.com>
2025-04-03 15:36:42 +0530

    day2

---
+++ Datatypes.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       byte age = 10;
+       short s = 32767;
+       int num = 2000000;
+       long  phoneNo = 8048489332l;
+       float f= 12.1212f;
+       double d =12.1212d;
+       boolean t = true;
+       char c= 'n';
+       System.out.println(age);
+       System.out.println(s);
+       System.out.println(num);
+       System.out.println(phoneNo);
+       System.out.println(f);
+       System.out.println(d);
+       System.out.println(t);
+       System.out.println(c);
+       
+	}
+
+}
