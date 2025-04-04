bf98f9be4cc08509af4501fc91bc65b564ab9a82
HarishRomario <155374957+HarishRomario@users.noreply.github.com>
2025-04-04 15:26:58 +0530

    day 3

---
+++ VariableAndOperation.java
@@ -0,0 +1,14 @@
+package day3;
+
+import java.util.Scanner;
+
+public class VariableAndOperation {
+
+	public static void main(String[] args) {
+	Scanner name = new Scanner(System.in);
+	String hello = name.nextLine();
+	System.out.println(hello);
+
+	}
+
+}
