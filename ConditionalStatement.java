b1fce03d65b0c7527ce239f391f3d8efa39015dc
HarishRomario <155374957+HarishRomario@users.noreply.github.com>
2025-04-08 11:31:17 +0530

    DAY5

---
+++ ConditionalStatement.java
@@ -0,0 +1,20 @@
+package day5;
+
+import java.util.Scanner;
+
+public class ConditionalStatement {
+	public static void main(String []arg) {
+		Scanner mark=new Scanner(System.in);
+		System.out.println("Enter the mark: ");
+		int sub=mark.nextInt();
+		if(35<=sub) {
+			System.out.println("pass");
+			
+			
+		}else {
+		System.out.println("fail");
+		}
+	}
+	
+
+}
