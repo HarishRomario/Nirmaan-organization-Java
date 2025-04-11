2c3894da1801b886add3e26ab55f25c6fd4675a1
HarishRomario <155374957+HarishRomario@users.noreply.github.com>
2025-04-11 19:50:12 +0530

    day 6

---
+++ ForloopEX.java
@@ -0,0 +1,17 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class ForloopEX {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the numeber: ");
+		
+		for(int i=1;i<=3;i++) {
+			System.out.println("raj");
+		}
+
+	}
+
+}
