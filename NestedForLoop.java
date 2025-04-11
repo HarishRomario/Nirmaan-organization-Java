38ee8b2b6f7d330ce9ae78dc2ad42e967678de7d
HarishRomario <155374957+HarishRomario@users.noreply.github.com>
2025-04-11 19:43:31 +0530

    day7

---
+++ NestedForLoop.java
@@ -0,0 +1,25 @@
+package day7;
+
+import java.util.Scanner;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the Row: ");
+		int a = sc.nextInt();
+		System.out.println("Enter the Coloumn: ");
+		int b = sc.nextInt();
+		for(int i=1;i<=a;i++) {
+			System.out.print(" ");
+			for(int j=1;j<=b;j++) {
+				System.out.println(" # ");
+				
+				}
+			}
+			
+		}
+		
+
+	}
+
