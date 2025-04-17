59666c79c2fb9844dee547ded6f82f28b1c4bd78
HarishRomario <155374957+HarishRomario@users.noreply.github.com>
2025-04-17 06:21:51 +0530

    day6

---
+++ TypeCasting.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int a = 378;
+		byte b = (byte)a;
+		System.out.println(b);{
+			 
+			float f = 7.6f;
+			int n= (int)f;
+			System.out.println(n);
+		}
+
+	}
+
+}
