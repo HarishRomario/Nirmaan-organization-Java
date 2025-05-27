00d4a1f3b863ee885ede4f104a910baf3f7b6219
HARISH KUMAR <155374957+HarishRomario@users.noreply.github.com>
2025-05-27 17:59:34 +0530

    day8

---
+++ ClassAndObject.java
@@ -0,0 +1,14 @@
+package day8;
+
+public class ClassAndObject {
+    int rollNo;
+    String name;
+    public static void manin(String [] arg) {
+    	ClassAndObject st= new ClassAndObject();
+    	st.rollNo=9;
+    	st.name="vijay";
+    	System.out.println(st.rollNo);
+    }
+    
+
+}
